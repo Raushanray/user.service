26817938a9d685e7da1ea7e9111315d9f00c34a9
Raushanray <raushankuray1998@gmail.com>
2023-04-21 13:07:00 +0530

    Implement  Circut breaker using resilience4J with the user-service add some dependencies and configure with get single user APIs because in this APIs user call rating service and reating-service calls hotel service internally.. this implements using application.yml file...

--- src/main/java/com/lcwr/user/service/controllers/UserController.java
+++ src/main/java/com/lcwr/user/service/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.lcwr.user.service.controllers;
 
 import com.lcwr.user.service.entities.User;
 import com.lcwr.user.service.services.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,12 +12,15 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
 
     //create
@@ -34,10 +40,20 @@ public class UserController {
 
     //get single user
     @GetMapping("/{userId}")
+    @CircuitBreaker(name = "ratingHotelBreaker",fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getSingleUser(@PathVariable("userId") String userId) {
+        logger.info("Get Single User Handler: UserController");
         User user = userService.getUser(userId);
         return ResponseEntity.ok(user);
     }
-
+//creating fall back  method for circuitbreaker
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex){
+        logger.info("fallback is executed because service is down :", ex.getMessage());
+        User user = User.builder().email("dummy@gmail.com").name("Dummy")
+                .about("This user is created dummy because some service is down")
+                .userId("141234")
+                .build();
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
 
 }
